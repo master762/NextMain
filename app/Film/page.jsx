46ebcb4758e90ec509a6d732417e7fb9607e3720
master762 <m8069248@gmail.com>
2025-02-19 23:49:46 +0300

    призма

--- app/Film/page.jsx
+++ app/Film/page.jsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import TheFooter from "@/components/TheFooter";
 import TheHeader from "@/components/TheHeader";
+import Genres from "@/components/Genres";
 
 export default function FilmPage() {
   return (
     <>
       <TheHeader showTextContent={false} showBgImage={false} />
       <Hero />
+      <Genres />
       <TheFooter />
     </>
   );
---
+++ app/api/categories/route.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const categories = await prisma.category.findMany();
+
+    // Преобразуем JSON-строку в массив
+    const formattedCategories = categories.map((category) => ({
+      ...category,
+      images: JSON.parse(category.images),
+    }));
+
+    return NextResponse.json(formattedCategories);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch categories" },
+      { status: 500 }
+    );
+  }
+}
