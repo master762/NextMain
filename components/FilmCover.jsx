e824371470222127e6c44b527b882d779622db48
master762 <m8069248@gmail.com>
2025-02-25 23:14:36 +0300

    удалены большие видеофайлы

--- components/FilmCover.jsx
+++ components/FilmCover.jsx
@@ -6,7 +6,7 @@ export default function FilmCover() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [quality, setQuality] = useState("1080p");
   const videoRef = useRef(null);
-
+  // массив с видео
   const videoSources = {
     "1080p": "/video/video1080p.mp4",
     "720p": "/video/video720p.mp4",
